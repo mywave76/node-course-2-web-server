eadeea21cc6949f86e1b12812b265464e98ea5a1
My Wave <mywave.reloaded@yahoo.com>
2018-04-18 23:34:24 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
